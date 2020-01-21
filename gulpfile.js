b19da85044bd5bf8ec0594c6a1fc9ef88fb1164f
ghranekUM <ghranek@uppermedic.com>
2020-01-21 09:15:36 +0200

    fix

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-var rtlcss = require("gulp-rtlcss");
-
-gulp.task("default", function() {
-  return gulp
-    .src("css/style.css")
-    .pipe(rtlcss())
-    .pipe(gulp.dest("rtl"));
-});
